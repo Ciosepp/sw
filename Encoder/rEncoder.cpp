81f98f1936cef1e9b41e2732fa2b49f23e9e1e8b
Unknown <electro.syst8@gmail.com>
2022-12-12 15:01:31 +0100

    Auto stash before rebase of "origin/master"

--- Encoder/rEncoder.cpp
+++ Encoder/rEncoder.cpp
@@ -7,14 +7,17 @@ enum inputType
 	INPUT
 };
 
+inputType inv;
+
 rEncoder :: rEncoder(uint8_t pinA,
 					 uint8_t pinB,
 					 uint8_t pinPB,
 					  bool EN_STATE){
 
-	pinMode(pinA, inputType[EN_STATE]);
-	pinMode(pinB, inputType[EN_STATE]);
-	pinMode(pinPB, inputType[EN_STATE]);
+	pinMode(pinA, inv[EN_STATE]);
+	pinMode(pinB, inv[EN_STATE]);
+	pinMode(pinPB, inv[EN_STATE]);
+
 	Apin = pinA;
 	BPin = pinB;
 	PBpin = pinPB;
@@ -23,14 +26,14 @@ rEncoder :: rEncoder(uint8_t pinA,
 	oldPress = digitalRead(PBpin);
 }
 
-int rEncoder :: updateEncoder(int oldVal)
-{
+int rEncoder :: updateEncoder(int oldVal){
+
 	static int oldA = HIGH; //set the oldA as HIGH
 	static int oldB = HIGH; //set the oldB as HIGH
 	int result = 0;
 	int newA = !digitalRead(Apin); //read the value of APin to newA
 	int newB = !digitalRead(Bpin); //read the value of BPin to newB
-	if (newA != oldA || newB != oldB)//if the value of APin or the BPin has changed
+	if (newA ^^ oldA || newB ^^ oldB)//if the value of APin or the BPin has changed
 	{  
 		if (oldA == HIGH && newA == LOW)// something has changed
 		{
@@ -39,7 +42,8 @@ int rEncoder :: updateEncoder(int oldVal)
 	}
 	oldA = newA;
 	oldB = newB;
-	return n+result;
+
+	return oldVal+result;
 }
 bool rEncoder :: getPBState()
 {
